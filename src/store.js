7f5cc3202e8c2c0f00dc76ee4d2dc01feb421587
Robin Malfait <malfait.robin@gmail.com>
2016-07-29 23:50:15 +0200

    add redux store

---
+++ src/store.js
@@ -0,0 +1,34 @@
+import {createStore} from 'redux';
+import throttle from 'lodash/throttle';
+
+import {loadState, saveState} from './storage';
+
+// import the root reducer
+import rootReducer, { defaultState } from './reducers/index';
+
+const store = createStore(
+  rootReducer,
+  loadState(defaultState)
+);
+
+// Hot Module Replacement
+if(module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+const saveFilteredState = throttle(() => {
+  requestAnimationFrame(() => saveState(store.getState()))
+}, 1000);
+
+store.subscribe(() => saveFilteredState());
+
+window.state = () => {
+  return store.getState()
+};
+
+export const getState = () => store.getState();
+export const {dispatch} = store;
+export default store;
