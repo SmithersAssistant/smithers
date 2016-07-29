4a5a2a40eba7a998784f48947f7ae046045e740e
Robin Malfait <malfait.robin@gmail.com>
2016-07-30 01:16:56 +0200

    add a little Event system thingy

---
+++ src/Event.js
@@ -0,0 +1,33 @@
+import {v4 as uuid} from 'uuid';
+
+class Event {
+  constructor() {
+    this.events = []
+  }
+
+  on(event, cb) {
+    const id = uuid()
+
+    this.events[event] = this.events[event] || []
+    this.events[event].push({id, cb})
+
+    // unsubscribe
+    return () => {
+      this.events[event] = this.events[event].filter(item => item.id !== id)
+    }
+  }
+
+  fire(event, ...args) {
+    (this.events[event] || []).map(({cb}) => cb(...args))
+  }
+}
+
+export default new Event()
+
+/**
+ * Events
+ */
+export const FOCUS_INPUT = 'FOCUS_INPUT'
+export const PUT_INPUT = 'PUT_INPUT'
+export const NOTIFY = 'NOTIFY'
+export const OPEN_SETTINGS  = 'OPEN_SETTINGS'
