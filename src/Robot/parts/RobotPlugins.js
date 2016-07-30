16057a91ed290f9ceb4d8cc493fa44fd04b5982e
Robin Malfait <malfait.robin@gmail.com>
2016-07-31 00:24:12 +0200

    parse the usage field, it will extract arguments (required) and optionals

--- src/Robot/parts/RobotPlugins.js
+++ src/Robot/parts/RobotPlugins.js
@@ -3,6 +3,41 @@ import pluginManager from 'PluginSystem/PluginManager'
 let plugins = [];
 let currentPlugin;
 
+const humanize = (str) => {
+  return str.split('_').join(' ')
+};
+
+const execAll = (regex, str) => {
+  var match;
+  var matches = [];
+  var isGlobal = regex.global;
+
+  while (match = regex.exec(str)) {
+    matches.push({
+      match: match[0],
+      contents: match.slice(1).join(),
+      humanized: humanize(match.slice(1).join()),
+      index: match.index
+    })
+
+    if (!isGlobal) {
+      break;
+    }
+  }
+
+  return matches;
+}
+
+const parseUsage = (usageString) => {
+  const ARGUMENTS_REGEX = /<([a-zA-Z0-9_]*)>/gi; // <name>
+  const OPTIONALS_REGEX = /<([a-zA-Z0-9_]*)\?>/gi; // <name?>
+
+  return {
+    arguments: [].concat(execAll(ARGUMENTS_REGEX, usageString)),
+    optionals: [].concat(execAll(OPTIONALS_REGEX, usageString))
+  }
+};
+
 export default {
   registerPlugin(plugin) {
     plugins = [
@@ -14,6 +49,7 @@ export default {
     ]
     currentPlugin = plugin
   },
+
   listen(regex, {description, usage}, cb) {
     plugins = plugins.map((plugin) => {
       if (plugin.name === currentPlugin.name) {
@@ -29,6 +65,7 @@ export default {
       return plugin;
     });
   },
+
   test(plugin, command) {
     plugins
       .find(p => p.name === plugin.name)
@@ -39,12 +76,14 @@ export default {
         matches: plugin.regex.exec(command)
       }));
   },
+
   commands(plugin) {
     return plugins
       .find(p => p.name === plugin.name)
       .commands
-      .map(({regex: name, description, usage}) => ({name, description, usage}));
+      .map(({regex: name, description, usage}) => ({name, description, usage, ...parseUsage(usage)}));
   },
+
   plugins() {
     return pluginManager.list().map(plugin => ({
       ...plugin,
