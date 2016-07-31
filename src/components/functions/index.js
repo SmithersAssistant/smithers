d75adbfe2b8bfe6a5cf8898532db46d632aa876b
Robin Malfait <malfait.robin@gmail.com>
2016-07-31 14:58:39 +0200

    add fuzzysearch functionality

--- src/components/functions/index.js
+++ src/components/functions/index.js
@@ -9,3 +9,33 @@ export const deleteProps = (component, toBeDeleted) => {
 
   return returnProps
 }
+
+
+/**
+ * Copyright https://github.com/bevacqua/fuzzysearch
+ */
+export const fuzzysearch = (needle, haystack) => {
+  var hlen = haystack.length;
+  var nlen = needle.length;
+
+  if (nlen > hlen) {
+    return false;
+  }
+
+  if (nlen === hlen) {
+    return needle === haystack;
+  }
+
+  outer: for (var i = 0, j = 0; i < nlen; i++) {
+    var nch = needle.charCodeAt(i);
+    while (j < hlen) {
+      if (haystack.charCodeAt(j++) === nch) {
+        continue outer;
+      }
+    }
+
+    return false;
+  }
+
+  return true;
+};
