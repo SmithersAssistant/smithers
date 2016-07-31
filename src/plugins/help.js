2612a04c3f73aa92a36fc8d2bae76dfe408c41af
Robin Malfait <malfait.robin@gmail.com>
2016-07-31 12:48:20 +0200

    parseUsage in the help file, underline the arguments with the current theme color it will also underline optional arguments with a light version of the current theme color

--- src/plugins/help.js
+++ src/plugins/help.js
@@ -4,11 +4,55 @@ const HELP_COMPONENT = 'com.robinmalfait.help';
 
 export default robot => {
 
+  const {
+    color
+  } = robot.UI;
   const {Table} = robot.cards;
 
   const Help = React.createClass({
     countPlugins() {
       return robot.plugins().length
+    },
+    parseUsage({usage, arguments: args, optionals}) {
+      let result = usage.split(' ');
+
+      if (args.length > 0) {
+        args.forEach(arg => {
+          result = result.map(item => {
+            if (item === arg.match) {
+              return <span style={{
+                borderBottom: `2px solid ${color(Robot.getThemeColor())}`,
+                borderRadius: 4
+              }}>{arg.humanized}</span>
+            }
+
+            return item;
+          });
+        });
+      }
+
+      if (optionals.length > 0) {
+        optionals.forEach(arg => {
+          result = result.map(item => {
+            if (item === arg.match) {
+              return <span style={{
+                borderBottom: `2px solid ${color(Robot.getThemeColor(), 100)}`,
+                borderRadius: 4
+              }}>{arg.humanized}</span>
+            }
+
+            return item;
+          });
+        });
+      }
+
+      return (
+        <span>
+          {result.map((item, i) => <span key={i} style={{
+            marginRight: 4
+          }}>{item}</span>)}
+        </span>
+      )
     },
     renderPlugins() {
       const plugins = [];
@@ -18,7 +62,7 @@ export default robot => {
         plugin.commands.forEach(command => {
           plugins.push([
             showed ? '' : String(plugin.name),
-            String(command.name),
+            this.parseUsage(command),
             String(command.description)
           ])
 
@@ -35,7 +79,7 @@ export default robot => {
         <Table
           {...other}
           title="Help"
-          header={['Plugin Name', 'Action RegEx', 'Description']}
+          header={['Plugin Name', 'Usage', 'Description']}
           body={this.renderPlugins()}
           footer={[{
             colSpan: 3,
