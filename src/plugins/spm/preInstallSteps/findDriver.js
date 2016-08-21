227e4911ae30337fb4e93e9ffc497521f5ec109e
Robin Malfait <malfait.robin@gmail.com>
2016-08-22 01:06:10 +0200

    add pre install steps
    these steps will be executed for every driver when installing a plugin

---
+++ src/plugins/spm/preInstallSteps/findDriver.js
@@ -0,0 +1,20 @@
+import drivers from '../drivers'
+
+export default function findDriver (input, robot) {
+  return {
+    label: 'Find driver',
+    cb ({ chain, done, appendToOutput }) {
+      return chain
+        .then(() => {
+          appendToOutput(`Locating correct installation driver for '${input}'`)
+          return drivers.find(driver => {
+            const driverInfo = driver(input, robot)
+            appendToOutput(`\n- Testing for '${driverInfo.label}'`)
+
+            return driverInfo.test()
+          })
+        })
+        .then(done)
+    }
+  }
+}
---
+++ src/plugins/spm/preInstallSteps/loadDriver.js
@@ -0,0 +1,21 @@
+export default function loadDriver (input, robot) {
+  return {
+    label: 'Load driver',
+    cb ({ chain, done, appendToOutput, failed, registerStep }) {
+      return chain
+        .then((driver) => {
+          if (driver === undefined) {
+            failed(`No installation driver found for '${input}', how do we need to install this? :(`)
+          }
+
+          appendToOutput(`Loading driver '${driver.name}'`)
+
+          const driverInfo = driver(input, robot)
+          driverInfo.steps.reverse().map(step => registerStep(step))
+
+          return driverInfo
+        })
+        .then(done)
+    }
+  }
+}
