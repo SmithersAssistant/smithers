9c8487983af9d94de0f1f77cc1cba95170f463e7
Robin Malfait <malfait.robin@gmail.com>
2016-08-24 23:49:44 +0200

    cleanup plugin manager a bit

--- src/pluginSystem/pluginManager.js
+++ src/pluginSystem/pluginManager.js
@@ -4,7 +4,7 @@ import config from 'config'
 import mkdirp from 'mkdirp'
 import {exec} from 'child_process'
 import rimraf from 'rimraf'
-import {LOCAL_PLUGIN, EXTERNAL_PLUGIN} from './sources'
+import {EXTERNAL_PLUGIN} from './sources'
 import Plugin from './Plugin'
 import Event, {PLUGIN_INSTALLING, PLUGIN_INSTALLED, PLUGIN_DELETING, PLUGIN_DELETED} from 'Event'
 
@@ -133,32 +133,6 @@ class PluginManager {
     // npm outdated --json
   }
 
-  addLocalPlugin (location) {
-    const meta = window.require(resolve(location, 'package.json'))
-    const plugins = config.get('plugins.local')
-    const alreadyExists = plugins.filter(plugin => plugin.location === location).length > 0
-
-    if (alreadyExists || !this.isValidPluginPath(location)) {
-      return false
-    }
-
-    const plugin = {
-      name: meta.name,
-      location,
-      version: meta.version
-    }
-
-    plugins.push(plugin)
-    config.set('plugins.local', plugins)
-
-    this.register({
-      ...plugin,
-      source: LOCAL_PLUGIN
-    }, this.loadPlugin(location))
-
-    return plugin
-  }
-
   removeExternalPlugin (module) {
     let plugins = config.get('plugins.external')
 
@@ -218,18 +192,6 @@ class PluginManager {
     } catch (err) {
       return null
     }
-  }
-
-  isValidPluginPath (path) {
-    if (!fs.existsSync(path)) {
-      return false
-    } else if (!fs.statSync(path).isDirectory()) {
-      return false
-    } else if (!fs.existsSync(`${path}/package.json`)) {
-      return false
-    }
-
-    return true
   }
 
   register (info, cb) {
