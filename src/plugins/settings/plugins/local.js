6546eb2c2adb89fd1f92337c08cf823520c5d53c
Robin Malfait <malfait.robin@gmail.com>
2016-08-25 16:53:37 +0200

    an example on the local page

--- src/plugins/settings/plugins/local.js
+++ src/plugins/settings/plugins/local.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {css} from 'aphrodite'
+import {sep} from 'path'
+import {homedir} from 'os'
 import styles from './styles'
 
 import {
@@ -38,9 +40,12 @@ export default ({robot}) => {
           ))}
         </Collection>
       ) : (
-        <p>
-          There are no local plugins, add your first local plugin now!
-        </p>
+        <div>
+          <p>
+            There are no local plugins, add your first local plugin now!
+          </p>
+          <pre>install {`${homedir()}${sep}your_first_plugin`}</pre>
+        </div>
       )}
     </div>
   )
