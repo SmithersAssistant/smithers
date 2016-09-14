3fb47b2606ab4cf73520e7177adec9438b2bb135
Robin Malfait <malfait.robin@gmail.com>
2016-09-14 23:23:15 +0200

    cleanup Main.js drastically
    Remove all the restorable hacky stuff, just add an `getState` and `setState` property on the props of a component.
    Plugin developers can use those methods to get the state or save the state from/to localStorage (or any other storage engine).
    
    getState() and setState(state) are automatically bound per component, meaning that internally the id of the component is attached.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,57 +1,11 @@
 import React from 'react'
-import isEmpty from 'lodash/isEmpty'
-
 import pluginManager from 'pluginSystem/pluginManager'
 
 import {dispatch} from 'store'
-import {removeCard} from 'actions/index'
+import {removeCard, saveCardState} from 'actions/index'
 import Event, {OPEN_HELP} from 'Event'
 import {withStyles} from 'components/functions'
-
-const RestorableComponent = React.createClass({
-  getDefaultProps () {
-    return {
-      registerCard: () => {} //  NOOP
-    }
-  },
-  componentDidMount () {
-    const {component} = this.refs
-    const {cardContainer, registerCard, isFunctional} = this.props
-
-    if (!isFunctional) {
-      if (component && component.isMounted()) {
-        if (!isEmpty(cardContainer.state)) {
-          component.setState(cardContainer.state)
-        }
-      }
-
-      registerCard(cardContainer.id, {
-        getState () {
-          return component.state
-        }
-      })
-    }
-  },
-  componentWillUnmount () {
-    let {isFunctional, cardContainer, unRegisterCard} = this.props
-    if (!isFunctional) {
-      unRegisterCard(cardContainer.id)
-    }
-  },
-  render () {
-    let {Component, componentProps, isFunctional, __CARD_ID__} = this.props
-    componentProps = {
-      ...componentProps,
-      removeCard: () => dispatch(removeCard(__CARD_ID__))
-    }
-
-    return isFunctional ? <Component {...componentProps} /> : <Component {...componentProps} ref='component' />
-  }
-})
-
-const isFunctionalComponent = (Component) => {
-  return Component.length === 1
-}
+import {getStateForCard} from 'state'
 
 const NoCards = withStyles(({ theme, px }) => ({
   noCardsWrapper: {
@@ -81,87 +35,40 @@ const NoCards = withStyles(({ theme, px }) => ({
   </div>
 ))
 
-const Card = (cardContainer, registerCard, unRegisterCard) => {
-  const C = pluginManager.resolveComponent(cardContainer.card)
+const Card = (cardContainer) => {
+  const Component = pluginManager.resolveComponent(cardContainer.card)
 
-  if (!C) {
+  if (!Component) {
     return null
   }
 
-  const isFunctional = isFunctionalComponent(C)
+  const ID = cardContainer.id
 
   return (
-    <div key={cardContainer.id}>
-      <RestorableComponent
-        key={cardContainer.id}
-        __CARD_ID__={cardContainer.id}
-        Component={C}
-        componentProps={cardContainer.props}
-        cardContainer={cardContainer}
-        isFunctional={isFunctional}
-        registerCard={registerCard}
-        unRegisterCard={unRegisterCard}
+    <div key={ID}>
+      <Component
+        {...cardContainer.props}
+        removeCard={() => dispatch(removeCard(ID))}
+        getState={() => getStateForCard(ID)}
+        setState={(state) => dispatch(saveCardState(ID, state))}
       />
     </div>
   )
 }
 
 const Main = React.createClass({
-  getDefaultProps () {
-    return {
-      saveCardStates: () => {} // NOOP
-    }
-  },
-  componentWillMount () {
-    this.cards = []
-  },
-  componentDidMount () {
-    this.saveComponentStates()
-  },
-  componentWillUnmount () {
-    window.cancelAnimationFrame(this._retrieveState)
-  },
-  registerCard (id, {getState}) {
-    this.cards = [
-      ...this.cards,
-      {
-        id,
-        getState
-      }
-    ]
-  },
-  saveComponentStates () {
-    setTimeout(() => {
-      this._retrieveState = window.requestAnimationFrame(() => {
-        if (this.cards.length > 0) {
-          const cardStates = this.cards.map(card => ({
-            id: card.id,
-            state: card.getState()
-          }))
-
-          const filteredCardStates = cardStates.filter(data => data.state !== null)
-
-          if (filteredCardStates.length > 0 && JSON.stringify(filteredCardStates) !== this.oldCardStates) {
-            this.oldCardStates = JSON.stringify(filteredCardStates)
-            this.props.saveCardStates(filteredCardStates)
-          }
-        }
-
-        this.saveComponentStates()
-      })
-    }, 1000) // Every second
-  },
-  unRegisterCard (id) {
-    this.cards = this.cards.filter(c => c.id !== id)
-  },
   render () {
     let {cards, styles} = this.props
 
-    cards = cards.map((c) => Card(c, this.registerCard, this.unRegisterCard)).filter(c => c !== null)
+    cards = cards.map((c) => Card(c)).filter(c => c !== null)
 
     return (
       <div className={styles.cardsStyles}>
-        {cards.length > 0 ? cards : NoCards()}
+        {
+          cards.length > 0
+            ? cards
+            : NoCards()
+        }
       </div>
     )
   }
