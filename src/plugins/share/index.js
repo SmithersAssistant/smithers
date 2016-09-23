67fe072be8851ac1da70b17a6cca26c43b65c7aa
Robin Malfait <malfait.robin@gmail.com>
2016-09-24 00:57:15 +0200

    add a share plugin
    - this can start a share process (via toolbar -> share card)
    - this can receive a shared card (via focus on windows || input field)

---
+++ src/plugins/share/index.js
@@ -0,0 +1,59 @@
+import { getCardById } from 'state'
+import { clipboard } from 'electron'
+
+export default robot => {
+  const RECEIVE_REGEX = /^share receive (.*)$/
+
+  function inject (code) {
+    const {
+      plugin,
+      card,
+      props,
+      state,
+      id
+    } = JSON.parse(window.atob(code))
+
+    if (!robot.cardExists(id)) {
+      const pluginExists = robot.plugins().find(p => p.name === plugin)
+
+      if (!pluginExists) {
+        robot.execute(`install ${plugin}`)
+      }
+
+      robot.injectSharedCard(id, card, props, state)
+      clipboard.writeText('')
+    }
+  }
+
+  window.addEventListener('focus', () => {
+    const text = clipboard.readText()
+
+    if (RECEIVE_REGEX.test(text)) {
+      inject(RECEIVE_REGEX.exec(text)[1])
+    }
+  })
+
+  robot.listen(/^share initiate (.*)$/, {
+    description: 'share a card',
+    usage: 'share initiate <id>'
+  }, ({ matches }) => {
+    try {
+      const cardData = getCardById(matches.id)
+      const data = {
+        plugin: robot.plugins().find(plugin => plugin.cards.find(card => card.name === cardData.card)).name,
+        ...cardData
+      }
+      clipboard.writeText(`share receive ${window.btoa(JSON.stringify(data))}`)
+      robot.notify('The sharing code is in your clipboard, share it with your friend!')
+    } catch (err) {
+      robot.notify('We could not share this card at the moment')
+    }
+  })
+
+  robot.listen(RECEIVE_REGEX, {
+    description: 'this will show a shared card',
+    usage: 'share receive <code>'
+  }, ({ matches }) => {
+    inject(matches.code)
+  })
+}
